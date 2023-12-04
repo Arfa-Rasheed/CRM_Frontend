a1d11e250be91fde4cd02a983993f691c09514d8
Arfa-Rasheed <arfarasheed9@gmail.com>
2023-12-04 08:45:57 -0800

    updated

--- src/shared-component/CRM-Grid.jsx/index.jsx
+++ src/shared-component/CRM-Grid.jsx/index.jsx
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+// import { makeStyles } from '@mui/material/styles';
+// import Table from '@material-ui/core/Table';
+// import TableBody from '@material-ui/core/TableBody';
+// import TableCell from '@material-ui/core/TableCell';
+// import TableContainer from '@material-ui/core/TableContainer';
+// import TableHead from '@material-ui/core/TableHead';
+// import TableRow from '@material-ui/core/TableRow';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material//Paper';
 import './style.scss'
 import { Box, Stack, Tab, Tabs } from '@mui/material';
 
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
+// const useStyles = makeStyles({
+//   table: {
+//     minWidth: 650,
+//   },
+// });
 
 const CRMGrid = (props) => {
-  const classes = useStyles();
+  // const classes = useStyles();
   const [hoveredRowIndex, setHoveredRowIndex] = useState(null);
 
   const [value, setValue] = React.useState(0);
@@ -29,7 +35,7 @@ const CRMGrid = (props) => {
 
   return (
     <TableContainer component={Paper} style={{ borderTopLeftRadius: '64px', borderTopRightRadius: '64px', marginTop: '15px', backgroundColor: '#DADADA', marginLeft: '10px', marginRight: '10px',boxShadow:'none' }}>
-      <Table className={classes.table} stickyHeader aria-label="sticky table" sx={{ border: '2px solid red' }}>
+      <Table stickyHeader aria-label="sticky table" sx={{}}>
         <TableHead style={{ height: '20vh' }}>
           <TableRow >
             {props.gridHeader.length > 0 &&
