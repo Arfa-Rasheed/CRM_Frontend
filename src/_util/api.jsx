ffa5c8b1ec2e253f3b5f746cdee334d463d0a8f2
Arfa-Rasheed <arfarasheed9@gmail.com>
2024-01-01 13:36:48 -0800

    baseurl changed

--- src/_util/api.jsx
+++ src/_util/api.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const httpClient = axios.create({
     // baseURL: "http://localhost:5000/api/",
-    baseURL:"crm-backend-7vj1.vercel.app/api/"
+    // baseURL:"crm-backend-7vj1.vercel.app/api/"
+    baseURL:"crm-backend-rosy.vercel.app/api/"
 });
 
 httpClient.interceptors.request.use(function (config) {
