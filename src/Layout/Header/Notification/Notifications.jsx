4fbaa5e3c787da707c15412496d502dfdc03de49
Arfa-Rasheed <arfarasheed9@gmail.com>
2024-05-27 21:06:46 +0500

    updated

--- src/Layout/Header/Notification/Notifications.jsx
+++ src/Layout/Header/Notification/Notifications.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
-import { MenuItem, Popover, Stack, Typography } from '@mui/material';
+import { Box, MenuItem, Popover, Stack, Typography } from '@mui/material';
 import './style.scss'
 import httpClient from '../../../_util/api';
 import { Bell } from 'phosphor-react';
@@ -13,11 +13,14 @@ const Notifications = (props) => {
   const navigate = useNavigate()
   const [notifications, setNotifications] = useState([
     {
+      id: "",
       message: "",
       status: 0,
-      policyNumber: ""
+      policyNumber: "",
+      unRead: true,
     }
   ]);
+  const [noOfUnReadNotifications, setNoOfUnReadNotifications] = useState(0)
 
   const isAdmin = JSON.parse(localStorage.getItem('isAdmin'))
 
@@ -30,18 +33,11 @@ const Notifications = (props) => {
     setAnchorEl(null);
   };
 
-  const handleOptionChange = (policyNumber) => {
-    // setAnchorEl(null);
-    // console.log();
-    // console.log("notification",notifications)
+  const handleOptionChange = (policyNumber, unRead, id) => {
+    const res = httpClient.post(`/notifications/updateNotification/${id}`).catch((error) => {
+      console.log(error);
+    })
     navigate(`/policyDetail/${policyNumber}`)
-    //  navigate(`/policyDetail/0900785190`)
-    // navigate('/policies')
-    // if (props.onOptionChange) {
-    //   props.onOptionChange(selectedOption)
-    //   // navigate(`/policyDetail/${notifications.policyNumber}`)
-    //   // navigate('/policies')
-    // }
   };
 
   const getNotifications = async () => {
@@ -50,20 +46,25 @@ const Notifications = (props) => {
       console.log("res", res);
       if (res?.status === 200) {
         console.log("res", res.data);
-        setNotifications(res.data.map(notifications => ({
+        setNotifications(res.data.notifications.map(notifications => ({
+          id: notifications._id,
           message: notifications.message,
           policyNumber: notifications.policyNumber,
-          status: notifications.status
+          status: notifications.status,
+          unRead: notifications.unRead,
         })))
+        setNoOfUnReadNotifications(res.data.noOfUnReadNotification)
       }
     }
     else {
       const res = await httpClient.get(`/notifications/getAllNotifications_AgentView/${agentCode}`).catch((error) => { console.log(error) })
       if (res?.status === 200) {
-        setNotifications(res.data.map(notifications => ({
+        setNotifications(res.data.notifications.map(notifications => ({
+          id: notifications._id,
           message: notifications.message,
           policyNumber: notifications.policyNumber,
-          status: notifications.status
+          status: notifications.status,
+          unRead: notifications.unRead
         })))
       }
     }
@@ -74,61 +75,78 @@ const Notifications = (props) => {
   }, [])
 
   return (
-    <div style={{
-      display: 'flex', alignItems: 'center', justifyContent: 'center', width: '4%', height: '6vh', backgroundColor: anchorEl ? '#F08613' : 'white', borderRadius: '30px',
-      '&:hover': {
-        backgroundColor: "#F08613",
-        cursor: 'pointer'
-      }
-    }}
-    >
-      {
-        anchorEl ? (<NotificationsIcon sx={{ color: 'white', fontSize: '31px' }} onClick={handleClick} />)
-          : (<NotificationsIcon sx={{ color: '#404040', fontSize: '31px' }} onClick={handleClick} />)
-      }
-      <Popover
-        anchorEl={anchorEl}
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-        sx={{
-          width: '30%',
-          height: '30vh'
-        }}
-        className='notification-popup'
+    <Stack flexDirection={'row'} sx={{
+      width: '49px'
+    }}>
+
+      <div style={{
+        display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', height: '6vh', backgroundColor: anchorEl ? '#F08613' : 'white', borderRadius: '30px',
+        '&:hover': {
+          backgroundColor: "#F08613",
+          cursor: 'pointer'
+        }
+      }}
       >
-        {notifications.length > 0 &&
-          notifications.slice().reverse().map((item, index) => {
-            return (
-              <MenuItem
-                className='menu-item'
-                sx={{
-                  display:'flex',
-                  flexWrap:'wrap',
-                  margin: '2px',
-                  height:'auto',
-                  // paddingTop: '12px',
-                  // paddingBottom: '12px',
-                  backgroundColor: item.status === 0 ? "#F08613" : "#DADADA",
-                  borderRadius: '13px',
-                  '&:hover': {
-                    backgroundColor: item.status === 0 ? "#F08613" : "#DADADA",
-                    cursor: 'pointer'
-                  }
-                }}
-                key={index}
-                onClick={() => handleOptionChange(item.policyNumber)}
-              >
-                <Stack sx={{width:"100px"}}>
-                  <Typography sx={{ fontSize: '14px' }}>{item.message}</Typography>
-                </Stack>
-                {/* <Typography sx={{ fontSize: '14px' }}>{item.message}</Typography> */}
-              </MenuItem>
-            );
-          })
+        {
+          anchorEl ? (<NotificationsIcon sx={{ color: 'white', fontSize: '31px' }} onClick={handleClick} />)
+            : (<NotificationsIcon sx={{ color: '#404040', fontSize: '31px' }} onClick={handleClick} />)
         }
 
-      </Popover>
-    </div>
+        <Popover
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+          sx={{
+            width: '25%',
+            height: '30vh'
+          }}
+          className='notification-popup'
+        >
+          {notifications.length > 0 &&
+            notifications.slice().reverse().map((item, index) => {
+              return (
+                <Stack
+                  className='menu-item'
+                  alignItems={'center'}
+                  justifyContent={'center'}
+                  sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    margin: '2px',
+                    width: '100%',
+                    height: '8vh',
+                    // paddingTop: '12px',
+                    // paddingBottom: '12px',
+                    backgroundColor: item.unRead ? "#F08613" : "#DADADA",
+                    borderRadius: '13px',
+                    '&:hover': {
+                      backgroundColor: item.unRead ? "#F08613" : "#DADADA",
+                      cursor: 'pointer'
+                    }
+                  }}
+                  key={index}
+                  onClick={() => handleOptionChange(item.policyNumber, item.unRead, item.id)}
+                >
+                  <Stack alignItems={'center'} justifyContent={'center'} sx={{ width: "85%" }}>
+                    <Typography sx={{ fontSize: '14px' }}>{item.message}</Typography>
+                  </Stack>
+                </Stack>
+              );
+            })
+          }
+
+        </Popover>
+      </div>
+      <Stack alignItems={'center'} justifyContent={'center'} sx={{
+        display:noOfUnReadNotifications > 0 ? 'flex' : 'none',
+        color: 'white', backgroundColor: "#F08613", marginTop: '-3px', marginLeft: '-16px', height: '18px', width: '21px', borderRadius: '26px', }}>
+        <Typography fontSize={'9px'}>
+          {noOfUnReadNotifications}
+          </Typography>
+      </Stack>
+    </Stack>
+
+
   )
 }
 
--- src/Layout/Header/index.js
+++ src/Layout/Header/index.js
@@ -1,16 +1,66 @@
 import { AppBar, Divider, Stack, Toolbar } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Profile from './Profile'
 import Notifications from './Notification/Notifications'
 import Logo from './Logo'
+import httpClient from '../../_util/api'
 
 const Header = () => {
+
+    // const isAdmin = JSON.parse(localStorage.getItem('isAdmin'))
+    // const [notifications, setNotifications] = useState([
+    //     {
+    //         id: "",
+    //         message: "",
+    //         status: 0,
+    //         policyNumber: "",
+    //         unRead: true,
+    //     }
+    // ]);
+
+    // const [noOfUnReadNotifications, setNoOfUnReadNotifications] = useState(0)
+
+    // const getNotifications = async () => {
+    //     if (isAdmin) {
+    //         const res = await httpClient.get('/notifications/getAllNotifications_AdminView').catch((error) => { console.log(error) })
+    //         console.log("res", res);
+    //         if (res?.status === 200) {
+    //             console.log("res", res.data);
+    //             setNotifications(res.data.notifications.map(notifications => ({
+    //                 id: notifications._id,
+    //                 message: notifications.message,
+    //                 policyNumber: notifications.policyNumber,
+    //                 status: notifications.status,
+    //                 unRead: notifications.unRead,
+    //             })))
+    //             setNoOfUnReadNotifications(res.data.noOfUnReadNotification)
+    //         }
+    //     }
+    //     else {
+    //         const res = await httpClient.get(`/notifications/getAllNotifications_AgentView/${agentCode}`).catch((error) => { console.log(error) })
+    //         if (res?.status === 200) {
+    //           setNotifications(res.data.notifications.map(notifications => ({
+    //             id: notifications._id,
+    //             message: notifications.message,
+    //             policyNumber: notifications.policyNumber,
+    //             status: notifications.status,
+    //             unRead:notifications.unRead
+    //           })))
+    //         }
+    //       }
+    // }
+
+    // useEffect(() => {
+    //     getNotifications()
+    // }, [])
+
+
     return (
         <div>
-            <AppBar sx={{ 
-                 backgroundColor: '#000000',
+            <AppBar sx={{
+                backgroundColor: '#000000',
                 //  border: '2px solid red' 
-                 }}>
+            }}>
                 <Toolbar>
                     <Stack
                         direction={'row'}
@@ -18,22 +68,22 @@ const Header = () => {
                         justifyContent="space-between"
                         spacing={4}
                         sx={{ width: '100%' }}>
-                        
 
-                        <Stack 
+
+                        <Stack
                             direction={'row'}
                             alignItems="center"
                             justifyContent="space-between"
                             sx={{
                                 // border:'2px solid green',
-                                width:'85%'
+                                width: '85%'
                             }}
-                            >
+                        >
                             <Logo />
                             <Notifications />
                         </Stack>
-                        
-                    
+
+
 
                         <Profile />
                     </Stack>
@@ -44,5 +94,4 @@ const Header = () => {
         </div>
     )
 }
-
 export default Header
